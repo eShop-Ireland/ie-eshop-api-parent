dff6bdea145002584a439d3beb489f9211a9d69b
iamamittank <iamamittank@gmail.com>
2019-12-14 16:42:51 +0000

    mongodb integration added

--- ie-eshop-api-web/src/main/java/ie/eshop/api/web/Application.java
+++ ie-eshop-api-web/src/main/java/ie/eshop/api/web/Application.java
@@ -3,11 +3,13 @@ package ie.eshop.api.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(
         exclude = SecurityAutoConfiguration.class,
         scanBasePackages = "ie.eshop.api"
 )
+@EnableMongoRepositories(basePackages = "ie.eshop.api.business.repository")
 public class Application {
 
     public static void main(String[] args) {
